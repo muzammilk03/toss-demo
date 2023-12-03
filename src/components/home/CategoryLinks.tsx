d7f14e6fa5ac8fc86d810d9c4e1c191f005e8939
Shabin-k <helloshabink@gmail.com>
2023-12-04 00:37:43 +0530

    feat : change

--- src/components/home/CategoryLinks.tsx
+++ src/components/home/CategoryLinks.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export const revalidate = 60 * 60 * 24;
 
-const LinkItem = ({ href, title }: { href: string; title: string }) => {
+const LinkItem = ({ title }: { title: string }) => {
   return (
     <Link
-      href={href}
+      href="/search"
       className=" relative flex w-fit flex-nowrap items-center gap-2 whitespace-nowrap rounded-3xl bg-white  px-4 py-2.5 text-center text-sm shadow-xl shadow-accent-100/50  transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-accent-200/50  hover:ring-2 hover:ring-accent-200"
     >
       {title}
@@ -30,15 +30,9 @@ const CategoryLinks = async () => {
 
   return (
     <ul className="grid w-full  grid-flow-col  gap-6 overflow-auto p-4 py-8">
-      <LinkItem href={"/search"} title="All Jobs" />
+      <LinkItem title="All Jobs" />
       {categories?.map((category) => {
-        return (
-          <LinkItem
-            key={category.id}
-            href={`/search?category=${category.name}`}
-            title={category.name}
-          />
-        );
+        return <LinkItem key={category.id} title={category.name} />;
       })}
     </ul>
   );
